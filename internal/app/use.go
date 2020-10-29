a61b9ec7a25fefd7254efbbb92f8652a61618d3c
Sebastian Bernheim <Sebastian.Bernheim@fmr.com>
2020-10-29 17:03:38 -0400

    Fix alias reconnect command info message

--- internal/app/use.go
+++ internal/app/use.go
@@ -222,7 +222,7 @@ func (a *App) resolveAndCheckAlias(params *UseParams) error {
 		return ErrAliasAlreadyUsed
 	}
 
-	zap.S().Infow("Command to connect using this alias: kconnect to " + params.Alias)
+	zap.S().Infof("Command to reconnect using this alias: kconnect to %s", params.Alias)
 
 	return nil
 }
