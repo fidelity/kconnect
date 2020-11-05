22b935772d8b6b68602fd935a3800f0950ce15eb
Kelly, Sean <a567910@fmr.com>
2020-11-05 06:58:32 -0500

    add filter function

---
+++ pkg/utils/utils_test.go
@@ -0,0 +1,73 @@
+package utils
+
+import "testing"
+
+
+func Test_Filter(t *testing.T) {
+	testCases := []struct {
+		name            string
+		inputValue      string 
+		inputFilter     string
+		expect          bool
+	}{
+		{
+			name:            "Simple match",
+			inputValue:      "this is a simple string",
+			inputFilter:     "simple",
+			expect:          true,
+		},
+		{
+			name:            "Simple mismatch",
+			inputValue:      "this is a simple string",
+			inputFilter:     "isnt",
+			expect:          false,
+		},
+		{
+			name:            "Multiple match (*)",
+			inputValue:      "this is a simple string",
+			inputFilter:     "simple*this",
+			expect:          true,
+		},
+		{
+			name:            "Multiple mismatch (*)",
+			inputValue:      "this is a simple string",
+			inputFilter:     "string*isnt",
+			expect:          false,
+		},
+		{
+			name:            "Multiple match (whitespace)",
+			inputValue:      "this is a simple string",
+			inputFilter:     "string this",
+			expect:          true,
+		},
+		{
+			name:            "Multiple mismatch (whitespace)",
+			inputValue:      "this is a simple string",
+			inputFilter:     "string mismatch",
+			expect:          false,
+		},
+		{
+			name:            "Multiple match (* + whitespace)",
+			inputValue:      "this is a simple string",
+			inputFilter:     "string*this*string",
+			expect:          true,
+		},
+		{
+			name:            "Multiple mismatch (* + whitespace)",
+			inputValue:      "this is a simple string",
+			inputFilter:     "string*is a mismatch",
+			expect:          false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := SurveyFilter(tc.inputFilter, tc.inputValue, 0)
+			if actual != tc.expect {
+				t.Fatalf("expected %t but got %t", tc.expect, actual)
+			}
+		})
+	}
+
+
+}
