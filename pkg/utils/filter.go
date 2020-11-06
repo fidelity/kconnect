0242845e679bf3847a75d34344a386dd90e2ad68
Kelly, Sean <a567910@fmr.com>
2020-11-06 05:32:04 -0500

    linting

--- pkg/utils/filter.go
+++ pkg/utils/filter.go
@@ -7,8 +7,8 @@ import (
 
 // SurveyFilter a function for passing to AlecAivazis/survey, which will allow wildcards(*) and whitespace to be used for subfilter values
 func SurveyFilter(filter string, value string, index int) bool {
-	newString := regexp.MustCompile("[\\s]+").ReplaceAllString(filter, "*")
-	subFilters := strings.Split(newString, "*")
+	parsedFilter := regexp.MustCompile("[\\s]+").ReplaceAllString(filter, "*")
+	subFilters := strings.Split(parsedFilter, "*")
 	for _, s := range(subFilters) {
 		if !strings.Contains(value, s) && s != "" {
 			return false
